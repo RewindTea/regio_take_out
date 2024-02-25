e708ea5964ce9ceb3f0137a1ab587c3336612fd5
rewind <2690469189@qq.com>
2024-02-25 16:18:15 +0800

    退出登录功能

--- regio/src/main/java/com/hyt/regio/controller/EmployeeController.java
+++ regio/src/main/java/com/hyt/regio/controller/EmployeeController.java
@@ -20,17 +20,16 @@ import javax.servlet.http.HttpServletRequest;
 @RestController
 @RequestMapping("/employee")
 public class EmployeeController {
-
     @Autowired
     private EmployeeService employeeService;
-
-//    员工登录
+    /*
+    * 后台系统员工登录功能
+    * */
     @PostMapping("/login")
     private R<Employee> login(@RequestBody Employee employee, HttpServletRequest request) {
         //将页面提交的密码进行加密处理
         String password = employee.getPassword();
         password = DigestUtils.md5DigestAsHex(password.getBytes());
-
         //查询数据库中是否有提交的用户名
         LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<>();
         queryWrapper.eq(Employee::getUsername, employee.getUsername());
@@ -39,19 +38,23 @@ public class EmployeeController {
         if (emp == null) {
             return R.error("登录失败，无此用户");
         }
-
         //密码比对
         if (!emp.getPassword().equals(password)) {//不一致
             return R.error("密码不一致");
         }
-
         //查看状态status是否禁用
         if (emp.getStatus() == 0) {
             return R.error("该用户已被禁用");
         }
-
         //登录成功，存session
         request.getSession().setAttribute("employee", employee.getId());
         return R.success(emp);
+    }
+
+    /*退出登录功能*/
+    @PostMapping("/logout")
+    public R<String> logout(HttpServletRequest request) {
+        request.getSession().removeAttribute("employee");
+        return R.success("退出成功");
     }
 }
